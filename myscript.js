6855b6c515ed246ff9a1208c41b8f2217123f9d4
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-04 13:09:28 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,405 @@
+var myQuestions = [
+  {
+    question: "1. There <span style='text-decoration:underline'>isn't</span style='text-decoration:underline'> <span style='text-decoration:underline'>a lot  </span style='text-decoration:underline'>  gasoline in your <span style='text-decoration:underline'> gas </span style='text-decoration:underline'> tank. You <span style='text-decoration:underline'>had better</span style='text-decoration:underline'> fill it up."
+    ,
+    answers: {
+      a: "isn't",
+      b: 'a lot',
+      c: 'car',
+      d: 'had better'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "2.  <span style='text-decoration:underline'>One</span style='text-decoration:underline'> of the <span style='text-decoration:underline'>students</span style='text-decoration:underline'> in my class <span style='text-decoration:underline'>were</span style='text-decoration:underline'> <span style='text-decoration:underline'>absent</span style='text-decoration:underline'> yesterday.",
+    answers: {
+      a: 'One',
+      b: 'student',
+      c: 'were',
+      d: 'absent'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "3.  <span style='text-decoration:underline'>Smoking</span style='text-decoration:underline'> two packets of <span style='text-decoration:underline'>cigarettes</span style='text-decoration:underline'> <span style='text-decoration:underline'>per</span style='text-decoration:underline'> day <span style='text-decoration:underline'>are</span style='text-decoration:underline'> dangerous.",
+    answers: {
+      a: 'Smoking',
+      b: 'cigarettes',
+      c: 'per',
+      d: 'are'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "4.  <span style='text-decoration:underline'>All</span style='text-decoration:underline'> of the <span style='text-decoration:underline'>windows</span style='text-decoration:underline'> in our house <span style='text-decoration:underline'>was</span style='text-decoration:underline'> <span style='text-decoration:underline'>broken</span style='text-decoration:underline'> in the earthquake.",
+    answers: {
+      a: 'All',
+      b: 'windows',
+      c: 'was',
+      d: 'broken'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "5.  <span style='text-decoration:underline'>Every</span style='text-decoration:underline'> one of my <span style='text-decoration:underline'>friends</span style='text-decoration:underline'> <span style='text-decoration:underline'>are</span style='text-decoration:underline'> at the football <span style='text-decoration:underline'>match</span style='text-decoration:underline'>.",
+    answers: {
+      a: 'Every',
+      b: 'friends',
+      c: 'are',
+      d: 'match'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "6.  <span style='text-decoration:underline'>None</span style='text-decoration:underline'> of the books <span style='text-decoration:underline'>are</span style='text-decoration:underline'> <span style='text-decoration:underline'>available</span style='text-decoration:underline'> at the store <span style='text-decoration:underline'>right</span style='text-decoration:underline'> now. ",
+    answers: {
+      a: 'None',
+      b: 'are',
+      c: 'available',
+      d: 'right'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "7.  There <span style='text-decoration:underline'>are</span style='text-decoration:underline'> <span style='text-decoration:underline'>plenty</span style='text-decoration:underline'> of gasoline in the car’s tank. We <span style='text-decoration:underline'>can</span style='text-decoration:underline'> <span style='text-decoration:underline'>drive</span style='text-decoration:underline'> at least 500 kilometers.",
+    answers: {
+      a: 'are',
+      b: 'plenty',
+      c: 'can',
+      d: 'drive'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "8. <span style='text-decoration:underline'>There</span style='text-decoration:underline'> <span style='text-decoration:underline'>is</span style='text-decoration:underline'> <span style='text-decoration:underline'>several</span style='text-decoration:underline'> <span style='text-decoration:underline'>pens</span style='text-decoration:underline'> on the teacher’s desk.",
+    answers: {
+      a: 'There',
+      b: 'is',
+      c: 'several',
+      d: 'pens'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "9. Chinese <span style='text-decoration:underline'>have</span style='text-decoration:underline'> <span style='text-decoration:underline'>more</span style='text-decoration:underline'> than fifty thousand <span style='text-decoration:underline'>written</span style='text-decoration:underline'> <span style='text-decoration:underline'>characters</span style='text-decoration:underline'>.",
+    answers: {
+      a: 'have',
+      b: 'more',
+      c: 'written',
+      d: 'characters'
+
+    },
+    correctAnswer: 'a'
+  },
+  
+  {
+    question: "10. The United States of America <span style='text-decoration:underline'>consist</span style='text-decoration:underline'> <span style='text-decoration:underline'>of</span style='text-decoration:underline'> fifty <span style='text-decoration:underline'>separate</span style='text-decoration:underline'> <span style='text-decoration:underline'>states</span style='text-decoration:underline'>.",
+    answers: {
+      a: 'consist',
+      b: 'of',
+      c: 'separate',
+      d: 'states'
+
+    },
+    correctAnswer: 'a'
+  },
+  
+  {
+    question: "11. <span style='text-decoration:underline'>A student</span style='text-decoration:underline'> <span style='text-decoration:underline'>walked</span style='text-decoration:underline'> into the room. <span style='text-decoration:underline'>It</span style='text-decoration:underline'> was looking for <span style='text-decoration:underline'>the teacher</span style='text-decoration:underline'>. ",
+    answers: {
+      a: 'A student',
+      b: 'walked',
+      c: 'It',
+      d: 'the teacher'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "12.<span style='text-decoration:underline'>Good worker</span style='text-decoration:underline'> should <span style='text-decoration:underline'>always</span style='text-decoration:underline'> do <span style='text-decoration:underline'>his or her</span style='text-decoration:underline'> job <span style='text-decoration:underline'>properly</span style='text-decoration:underline'>.",
+    answers: {
+      a: 'Good worker',
+      b: 'always',
+      c: 'his or her',
+      d: 'properly'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "13. <span style='text-decoration:underline'>Anyone</span style='text-decoration:underline'> <span style='text-decoration:underline'>can speak</span style='text-decoration:underline'> English well if <span style='text-decoration:underline'>he/she</span style='text-decoration:underline'> <span style='text-decoration:underline'>want</span style='text-decoration:underline'> to.",
+    answers: {
+      a: 'Anyone',
+      b: 'can speak',
+      c: 'he/she',
+      d: 'want'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "14. <span style='text-decoration:underline'>My family</span style='text-decoration:underline'> <span style='text-decoration:underline'>are</span style='text-decoration:underline'> supportive and loving. <span style='text-decoration:underline'>They</span style='text-decoration:underline'> <span style='text-decoration:underline'>has</span style='text-decoration:underline'> always supported me.",
+    answers: {
+      a: 'My family',
+      b: 'are',
+      c: 'They',
+      d: 'has'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "15. The office <span style='text-decoration:underline'>staff</span style='text-decoration:underline'> gave <span style='text-decoration:underline'>his</span style='text-decoration:underline'> boss <span style='text-decoration:underline'>a nice gift</span> when <span style='text-decoration:underline'>he</span> retired.",
+    answers: {
+      a: 'staff',
+      b: 'his',
+      c: 'a nice gift',
+      d: 'he'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "16. I got two letters <span style='text-decoration:underline'>yesterday</span>. <span style='text-decoration:underline'>One</span> was from my brother. <span style='text-decoration:underline'>Another</span> was from <span style='text-decoration:underline'>my sister</span>.",
+    answers: {
+      a: 'yesterday',
+      b: 'One',
+      c: 'Another',
+      d: 'my sister'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "17. <span style='text-decoration:underline'>Some people</span> <span style='text-decoration:underline'>prefer</span> classical music, but <span style='text-decoration:underline'>the others</span> like <span style='text-decoration:underline'>pop music</span>. ",
+    answers: {
+      a: 'Some people',
+      b: 'prefer',
+      c: 'the others',
+      d: 'pop music'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "18. Twenty <span style='text-decoration:underline'>trainees</span> <span style='text-decoration:underline'>took</span> the test. Fifteen trainees passed the test, <span style='text-decoration:underline'>but</span> the <span style='text-decoration:underline'>others</span> trainees failed.",
+    answers: {
+      a: 'trainees',
+      b: 'took',
+      c: 'but',
+      d: 'tohers'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "19. <span style='text-decoration:underline'>A train and a bus</span> are <span style='text-decoration:underline'>means</span> of transportation. <span style='text-decoration:underline'>Another</span> <span style='text-decoration:underline'>are</span> the horse and <span style='text-decoration:underline'>the bike</span>.",
+    answers: {
+      a: "A train and a bus",
+      b: "means",
+      c: "Another",
+      d: 'the bike'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "20. In just <span style='text-decoration:underline'>other</span> <span style='text-decoration:underline'>three weeks</span>, he <span style='text-decoration:underline'>will be</span> a <span style='text-decoration:underline'>retired</span> Aramco employee.",
+    answers: {
+      a: "other",
+      b: "three weeks",
+      c: "will be",
+      d: 'retired'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "21. I thanked <span style='text-decoration:underline'>the teacher</span> <span style='text-decoration:underline'>whom</span> <span style='text-decoration:underline'>taught</span> me <span style='text-decoration:underline'>to speak</span> English.",
+    answers: {
+      a: "the teacher",
+      b: "whom",
+      c: "taught",
+      d: "to speak"
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "22. <span style='text-decoration:underline'>The book</span> <span style='text-decoration:underline'>whose</span> is <span style='text-decoration:underline'>on the table</span> <span style='text-decoration:underline'>is</span> a very useful one. ",
+    answers: {
+      a: "The book",
+      b: "whose",
+      c: "on the table",
+      d: "is"
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "23. The students <span style='text-decoration:underline'>which</span> <span style='text-decoration:underline'>sit</span> in the front row <span style='text-decoration:underline'>are</span> <span style='text-decoration:underline'>from</span> China.",
+    answers: {
+      a: "which",
+      b: "si",
+      c: "are",
+      d: "from"
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "24. I <span style='text-decoration:underline'>am using</span> <span style='text-decoration:underline'>a sentence</span> <span style='text-decoration:underline'>it</span> <span style='text-decoration:underline'>contains</span> an adjective clause.",
+    answers: {
+      a: "am using",
+      b: "a sentence",
+      c: "it",
+      d: "contains"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "25. <span style='text-decoration:underline'>The music</span> to <span style='text-decoration:underline'>which</span> we listened <span style='text-decoration:underline'>to</span> <span style='text-decoration:underline'>was</span> very good.",
+    answers: {
+      a: "The music",
+      b: "which",
+      c: "to",
+      d: "was"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "26. The lawyer <span style='text-decoration:underline'>told</span> <span style='text-decoration:underline'>me</span> <span style='text-decoration:underline'>that I</span> <span style='text-decoration:underline'>inherit</span> a lot of money at the end of the year.",
+    answers: {
+      a: "told",
+      b: "me",
+      c: "That I",
+      d: "inherit"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "27. I don’t watch T.V very much because there <span style='text-decoration:underline'>are</span> <span style='text-decoration:underline'>a few</span> <span style='text-decoration:underline'>television</span> <span style='text-decoration:underline'>programs</span> that I enjoy.",
+    answers: {
+      a: "are",
+      b: "a few",
+      c: "television",
+      d: "programs"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "28. He <span style='text-decoration:underline'>wondered</span> <span style='text-decoration:underline'>if</span> <span style='text-decoration:underline'>he</span> <span style='text-decoration:underline'>is</span> doing the right thing.",
+    answers: {
+      a: "wondered",
+      b: "if",
+      c: "he",
+      d: "is"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "29. I think that <span style='text-decoration:underline'>a few</span> <span style='text-decoration:underline'>lemon</span> <span style='text-decoration:underline'>juice</span> on fish <span style='text-decoration:underline'>makes</span> it taste better.",
+    answers: {
+      a: "a few",
+      b: "juice",
+      c: "makes",
+      d: "it"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "30. Let’s <span style='text-decoration:underline'>ask</span> him <span style='text-decoration:underline'>which one</span> <span style='text-decoration:underline'>does</span> he <span style='text-decoration:underline'>wants</span>.",
+    answers: {
+      a: "ask",
+      b: "which one",
+      c: "does",
+      d: "wants"
+    },
+    correctAnswer: 'd'
+  },
+
+  
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
